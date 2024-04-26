abe7ed3cbd9bfcccaa651a19aa370daf612b1f5c
xyehtz <alejitogarbo@gmail.com>
2024-04-26 14:56:11 -0400

    Code refactoring Created the new LoadSecrests.cs file that loads all the secrets of the bot. The secrets are used in the Program.cs with a reference variable Changed the path of the .env file

--- src/Program.cs
+++ src/Program.cs
@@ -10,29 +10,16 @@ public class Program {
     private static DiscordSocketClient _client;
 
     public static async Task Main() {
-        // Get Discord TOKEN using .env and DotNetEnv module
-        DotNetEnv.Env.Load(@".env");
-
-        // Get token and channel id from .env
-        string token = DotNetEnv.Env.GetString("TOKEN");
-        ulong applicationId = Convert.ToUInt64(DotNetEnv.Env.GetString("APPLICATION_ID"));
-        ulong guildId = Convert.ToUInt64(DotNetEnv.Env.GetString("GUILD_ID"));
-        ulong testChannelId = Convert.ToUInt64(DotNetEnv.Env.GetString("TEST_CHANNEL_ID"));
-        ulong modChannelId = Convert.ToUInt64(DotNetEnv.Env.GetString("MOD_CHANNEL_ID"));
-        ulong welcomeChannelId = Convert.ToUInt64(DotNetEnv.Env.GetString("TEST_CHANNEL_ID"));
-        ulong announcementsChannelId = Convert.ToUInt64(DotNetEnv.Env.GetString("ANNOUNCEMENTS_CHANNEL_ID"));
-        ulong generalChannelId = Convert.ToUInt64(DotNetEnv.Env.GetString("GENERAL_CHANNEL_ID"));
-        ulong testRoleId = Convert.ToUInt64(DotNetEnv.Env.GetString("TEST_ROLE_ID"));
-        ulong doofRoleId = Convert.ToUInt64(DotNetEnv.Env.GetString("DOOF_ROLE_ID"));
-
+        DotNetEnv.Env.Load(@"secrets/.env");
+        LoadSecrets loadSecrets = new LoadSecrets();
 
         var config = new DiscordSocketConfig() {
             GatewayIntents = 
                 GatewayIntents.All
         };
 
-        GuildEvents guildEvents =  new GuildEvents(announcementsChannelId, doofRoleId);
-        UserEvents userEvents = new UserEvents(welcomeChannelId, modChannelId);
+        GuildEvents guildEvents =  new GuildEvents(loadSecrets.getAnnouncementsChannelId(), loadSecrets.getDoofRoleId());
+        UserEvents userEvents = new UserEvents(loadSecrets.getWelcomeChannelId(), loadSecrets.getModChannelId());
 
         // Start Discord client and logging of the bot
         _client = new DiscordSocketClient(config);
@@ -48,13 +35,13 @@ public class Program {
         _client.GuildScheduledEventStarted += guildEvents.GuildScheduledEventStarted;
 
         // Login using the token and start the bot
-        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.LoginAsync(TokenType.Bot, loadSecrets.getToken());
         await _client.StartAsync();
 
         // Create a function to send a message to a channel when the bot is ready
         _client.Ready += async () => {
-            var botChannel = _client.GetChannel(testChannelId) as IMessageChannel;
-            var generalChannel = _client.GetChannel(generalChannelId) as IMessageChannel;
+            var botChannel = _client.GetChannel(loadSecrets.getTestChannelId()) as IMessageChannel;
+            var generalChannel = _client.GetChannel(loadSecrets.getGeneralChannelId()) as IMessageChannel;
 
             if (botChannel == null || generalChannel == null) {
                 Console.WriteLine("Error sending message to the channel(s)");
@@ -67,11 +54,5 @@ public class Program {
 
         // Block the task until the program is closed
         await Task.Delay(-1);
-    }
-
-    // Set logging
-    private static Task Log(LogMessage msg) {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
     }
 }
---
+++ src/secrets/LoadSecrets.cs
@@ -0,0 +1,12 @@
+public class LoadSecrets {
+    public Func<string> getToken = () => { return DotNetEnv.Env.GetString("TOKEN"); };
+    public Func<ulong> getApplicationId = () => {return Convert.ToUInt64(DotNetEnv.Env.GetString("APPLICATION_ID"));};
+    public Func<ulong> getGuildId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("GUILD_ID")); };
+    public Func<ulong> getTestChannelId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("TEST_CHANNEL_ID")); };
+    public Func<ulong> getModChannelId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("MOD_CHANNEL_ID")); };
+    public Func<ulong> getWelcomeChannelId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("WELCOME_CHANNEL_ID")); };
+    public Func<ulong> getAnnouncementsChannelId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("ANNOUNCEMENTS_CHANNEL_ID")); };
+    public Func<ulong> getGeneralChannelId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("GENERAL_CHANNEL_ID")); };
+    public Func<ulong> getTestRoleId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("TEST_ROLE_ID")); };
+    public Func<ulong> getDoofRoleId = () => { return Convert.ToUInt64(DotNetEnv.Env.GetString("DOOF_ROLE_ID")); };
+}
