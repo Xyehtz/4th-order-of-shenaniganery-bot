4539ca289596acd7de1394603807d30a008b90a5
xyehtz <alejitogarbo@gmail.com>
2024-05-04 20:01:32 -0400

    Fixed code conflict - Fixed code conflict between JingleModule.cs and AlbaniaModule.cs

--- src/modules/AlbaniaModule.cs
+++ src/modules/AlbaniaModule.cs
@@ -2,13 +2,13 @@ using Discord;
 using Discord.Commands;
 
 /// <summary>
-/// Class in charge of executing the Jingle command. This command will check if the user is in a voice channel, and play the jingle sound of Doofenshmirtz Evil Incorporated
+/// Class in charge of executing the alAnthem command. This command will check if the user is in a voice channel, and play the Albanian anthem
 /// </summary>
 public class AlbaniaModule : ModuleBase<SocketCommandContext> {
     ISendAudio sendAudio = new SendAudio();
 
     /// <summary>
-    /// This method will do the check if the user is on a voice channel, depending on that, it will send a message to the user if the user is not on a voice channel, if the user is on a voice channel, the bot will join and play the jingle sound by calling the SendAsync method of the sendAudio class
+    /// This method will do the check if the user is on a voice channel, depending on that, it will send a message to the user if the user is not on a voice channel, if the user is on a voice channel, the bot will join and play the Albanian anthem by calling the SendAsync method of the sendAudio class
     /// </summary>
     /// <param name="voiceChannel">
     /// The parameter voiceChannel is set automatically to null as it will change depending on the state of the user, this also means that the user won't need to send a parameter when using the command
@@ -24,6 +24,7 @@ public class AlbaniaModule : ModuleBase<SocketCommandContext> {
 
         var audioClient = await voiceChannel.ConnectAsync();
 
+        await Context.Channel.SendMessageAsync("So you want to hear the anthem of my country?");
         await sendAudio.SendAsync(audioClient, "audio/albania_anthem.mp3");
     }
 }
--- src/modules/JingleModule.cs
+++ src/modules/JingleModule.cs
@@ -8,7 +8,7 @@ public class JingleModule : ModuleBase<SocketCommandContext> {
     ISendAudio sendAudio = new SendAudio();
 
     /// <summary>
-    /// This method will do the check if the user is on a voice channel, depending on that, it will send a message to the user if the user is not on a voice channel, if the user is on a voice channel, the bot will join and play the Albanian anthem by calling the SendAsync method of the sendAudio class
+    /// This method will do the check if the user is on a voice channel, depending on that, it will send a message to the user if the user is not on a voice channel, if the user is on a voice channel, the bot will join and play the jingle sound by calling the SendAsync method of the sendAudio class
     /// </summary>
     /// <param name="voiceChannel">
     /// The parameter voiceChannel is set automatically to null as it will change depending on the state of the user, this also means that the user won't need to send a parameter when using the command
@@ -24,7 +24,6 @@ public class JingleModule : ModuleBase<SocketCommandContext> {
 
         var audioClient = await voiceChannel.ConnectAsync();
 
-        await Context.Channel.SendMessageAsync("So you want to hear the anthem of my country?");
         await sendAudio.SendAsync(audioClient, "audio/jingle.mp3");
     }
 }
